78a7de22de1ac4616167e00f0108ec40bdc03914
sazep <sazep7w@gmail.com>
2024-07-15 16:59:35 +0200

    update html add icon update style.css add lang.svg fix image

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
-console.clear();
+window.addEventListener('load', () => {
+  document.querySelector(body-load-after).style.overflowY = "hidden"
+  alert("vse ok")
+  setTimeout(2000,()=>{
+    document.body.style.overflowY = 'visible'
+    document.querySelector(load-main).style.display = "none"
+    alert("xz")
+  })
+})
+// следить за курсоро чтоб красиво менюшки переливались
+// console.clear()
 
-const cardsContainer = document.querySelector(".cards");
-const cardsContainerInner = document.querySelector(".cards__inner");
-const cards = Array.from(document.querySelectorAll(".card"));
-const overlay = document.querySelector(".overlay");
+let cardsContainer = document.querySelector(".cards")
+let cardsContainerInner = document.querySelector(".cards__inner")
+let cards = Array.from(document.querySelectorAll(".card"))
+let overlay = document.querySelector(".overlay")
 
-const applyOverlayMask = (e) => {
-  const overlayEl = e.currentTarget;
-  const x = e.pageX - cardsContainer.offsetLeft;
-  const y = e.pageY - cardsContainer.offsetTop;
+let applyOverlayMask = (e) => {
+  let overlayEl = e.currentTarget
+  let x = e.pageX - cardsContainer.offsetLeft
+  let y = e.pageY - cardsContainer.offsetTop
 
-  overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`;
-};
+  overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`
+}
 
-const createOverlayCta = (overlayCard, ctaEl) => {
-  const overlayCta = document.createElement("div");
-  overlayCta.classList.add("cta");
-  overlayCta.textContent = ctaEl.textContent;
-  overlayCta.setAttribute("aria-hidden", true);
-  overlayCard.append(overlayCta);
-};
+let createOverlayCta = (overlayCard, ctaEl) => {
+  let overlayCta = document.createElement("div")
+  overlayCta.classList.add("cta")
+  overlayCta.textContent = ctaEl.textContent
+  overlayCta.setAttribute("aria-hidden", true)
+  overlayCard.append(overlayCta)
+}
 
-const observer = new ResizeObserver((entries) => {
+let observer = new ResizeObserver((entries) => {
   entries.forEach((entry) => {
-    const cardIndex = cards.indexOf(entry.target);
-    let width = entry.borderBoxSize[0].inlineSize;
-    let height = entry.borderBoxSize[0].blockSize;
+    let cardIndex = cards.indexOf(entry.target)
+    let width = entry.borderBoxSize[0].inlineSize
+    let height = entry.borderBoxSize[0].blockSize
 
     if (cardIndex >= 0) {
-      overlay.children[cardIndex].style.width = `${width}px`;
-      overlay.children[cardIndex].style.height = `${height}px`;
+      overlay.children[cardIndex].style.width = `${width}px`
+      overlay.children[cardIndex].style.height = `${height}px`
     }
-  });
-});
+  })
+})
 
 const initOverlayCard = (cardEl) => {
-  const overlayCard = document.createElement("div");
-  overlayCard.classList.add("card");
-  createOverlayCta(overlayCard, cardEl.lastElementChild);
-  overlay.append(overlayCard);
-  observer.observe(cardEl);
-};
+  const overlayCard = document.createElement("div")
+  overlayCard.classList.add("card")
+  createOverlayCta(overlayCard, cardEl.lastElementChild)
+  overlay.append(overlayCard)
+  observer.observe(cardEl)
+}
 
-cards.forEach(initOverlayCard);
-document.body.addEventListener("pointermove", applyOverlayMask);
+cards.forEach(initOverlayCard)
+document.body.addEventListener("pointermove", applyOverlayMask)
